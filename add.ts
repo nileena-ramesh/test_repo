ea0e2e53ab6e42fe3da15b439cc6b1de3795c115
nileena-ramesh <nileena@neoito.com>
2022-02-24 15:59:31 +0530

    modified file

--- add.ts
+++ add.ts
@@ -1,7 +1,7 @@
 function addNumbers(a: number, b: number) { 
-    return a + b + c; 
+    return a + b;
 } 
 
-var sum: number = addNumbers(10, 15) 
+var sum: number = addNumbers("10", 15) ;
 
 console.log('Sum of the two numbers is: ' +sum); 
