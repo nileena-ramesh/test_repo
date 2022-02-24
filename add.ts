47380605df05af338dc137c4cd3693d0ef3992c9
nileena-ramesh <nileena@neoito.com>
2022-02-24 16:06:11 +0530

    modified file

--- add.ts
+++ add.ts
@@ -1,7 +1,18 @@
-function addNumbers(a: number, b: number) { 
+/*function addNumbers(a: number, b: number) { 
     return a + b;
 } 
 
 var sum: number = addNumbers("10", 15) ;
 
-console.log('Sum of the two numbers is: ' +sum); 
+console.log('Sum of the two numbers is: ' +sum); */
+
+function foo() {
+    try {
+        return 1; // We expect 1 to be returned
+    } catch(err) {
+        return 2; // Or 2 in cases of error
+    } finally {
+        return 3; // Noncompliant: 3 is returned before 1, or 2, which we did not expect
+    }
+}
+
