21ef3969af2e60f5113d77df899d37c6c78bb721
nileena-ramesh <nileena@neoito.com>
2022-02-24 15:39:14 +0530

    modified file

--- add.ts
+++ add.ts
@@ -5,3 +5,4 @@ function addNumbers(a: number, b: number) {
 var sum: number = addNumbers(10, 15) 
 
 console.log('Sum of the two numbers is: ' +sum); 
+console.log('Sum of the two numbers is: ' +sum); 
